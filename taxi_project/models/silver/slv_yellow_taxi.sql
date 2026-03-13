f3c7719b7086a0faf711d9d160ba05b0e2c1d620
Brad Coles <you@example.com>
2026-03-13 19:21:09 +1030

    Simplified model

--- taxi_project/models/silver/slv_yellow_taxi.sql
+++ taxi_project/models/silver/slv_yellow_taxi.sql
@@ -2,34 +2,27 @@ with source as (
 
     select * from {{ ref('brz_yellow_taxi') }}
 
-),
-
-cleaned as (
-
-    select
-        trip_id,
-        vendor_id,
-        pickup_datetime::timestamp                                                       as pickup_datetime,
-        dropoff_datetime::timestamp                                                      as dropoff_datetime,
-        passenger_count,
-        trip_distance,
-        pickup_location_id,
-        dropoff_location_id,
-        payment_type,
-        fare_amount,
-        tip_amount,
-        total_amount,
-
-        -- derived columns
-        datediff('minute', pickup_datetime::timestamp, dropoff_datetime::timestamp)      as trip_duration_minutes
-
-    from source
-
-    where
-        fare_amount > 0
-        and trip_distance > 0
-        and passenger_count > 0
-
 )
+select
+    trip_id,
+    vendor_id,
+    pickup_datetime::timestamp                                                       as pickup_datetime,
+    dropoff_datetime::timestamp                                                      as dropoff_datetime,
+    passenger_count,
+    trip_distance,
+    pickup_location_id,
+    dropoff_location_id,
+    payment_type,
+    fare_amount,
+    tip_amount,
+    total_amount,
 
-select * from cleaned
+    -- derived columns
+    datediff('minute', pickup_datetime::timestamp, dropoff_datetime::timestamp)      as trip_duration_minutes
+
+from source
+
+where
+    fare_amount > 0
+    and trip_distance > 0
+    and passenger_count > 0
